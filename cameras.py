82d8d95ac3eccbbf22bdf0ac9fac37deeae1d981
Filip Tomczyk <sq3tle@gmail.com>
2023-07-26 22:14:26 +0200

    Update cameras.py

--- cameras.py
+++ cameras.py
@@ -8,46 +8,13 @@ from picamera2.encoders import H264Encoder
 from picamera2.outputs import Output
 
 camera = int(sys.argv[1]) if sys.argv[1] else 0
-
+number = 0
 path = "/home/pi/camera{}/".format(camera)
 session_id = (''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))).upper()
-video_config = {"size": (1920, 1080)}
+
 
 print("camera:", camera)
 print("session_id:", session_id)
-
-
-class SplitOutput(Output):
-
-    def __init__(self, filename, fps=30):
-        super().__init__()
-        self._cfile = None
-        self._filename = filename
-        self._fps = fps
-        self._frame = 0
-        self._number = 0
-        self._last_frame_time = time.time()
-
-    def running(self):
-        return time.time() - self._last_frame_time < 1
-
-    def outputframe(self, frame, keyframe=True, timestamp=None):
-
-        self._last_frame_time = time.time()
-
-        if ((self._frame > self._fps * 10 and keyframe) or not self._cfile):
-
-            self._number += 1
-            self._frame = 0
-
-            if self._cfile:
-                self._cfile.close()
-
-            self._cfile = open("{}rec-{}_{}.h264".format(path, self._filename, str(self._number).zfill(6)), "ab")
-
-        self._cfile.write(frame)
-
-        self._frame += 1
 
 
 # CAMERA SETUP
@@ -60,20 +27,15 @@ except:
     GPIO.setup(16, GPIO.OUT)
     exit(10)
        
-cam.configure(cam.create_video_configuration(video_config))
-cam.encoder = H264Encoder(4000000, repeat=True)
-
-cam.encoder.output = SplitOutput(session_id)
-cam.start_encoder()
+camera_config = cam.create_still_configuration()
+cam.configure(camera_config)
 cam.start()
+cam.set_controls({"AfMode": 0, "LensPosition": 3.0})
+time.sleep(5)
+
 while 1:
-    if not cam.encoder.output.running():
-        print("CATASTROPHIC FAILURE - TIMEOUT")
-        import RPi.GPIO as GPIO
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(16, GPIO.OUT)
-        exit(10)
-    time.sleep(1)
+    number+=1
+    cam.capture_file("{}img-{}_{}.jpg".format(path, session_id, str(number).zfill(6)))
+    time.sleep(5)
 
 cam.stop()
-cam.stop_encoder()
